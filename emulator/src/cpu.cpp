71fbeb433348655e106747fa4fae580ea3c39415
DridriLaBastos <DridriLaBastos@users.noreply.github.com>
2020-05-15 14:19:56 +0200

    update the source tree architecture and delete the dynamic lib non sense

--- emulator/src/cpu.cpp
+++
@@ -1,6 +0,0 @@
-#include "cpu/8086.cpp"
-#include "cpu/instructions_logic.cpp"
-#include "cpu/instructions_arithmetic.cpp"
-#include "cpu/instructions_data_transfer.cpp"
-#include "cpu/instructions_control_transfer.cpp"
-#include "cpu/instructions_processor_control.cpp"
